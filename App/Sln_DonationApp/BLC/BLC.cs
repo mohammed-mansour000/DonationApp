c814293b9ee2571f2e83017535e6697df7713990
mohammed-mansour000 <hamzimansour123@gmail.com>
2021-12-11 20:01:37 +0200

    adding get users in blv

--- App/Sln_DonationApp/BLC/BLC.cs
+++ App/Sln_DonationApp/BLC/BLC.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using static DALC.IDALC;
 
 namespace BLC
 {
     public class BLC
     {
+        string ConnectionString = @"Data Source=DESKTOP-FI4HSGV\MANSURSQL;Initial Catalog=DonationAppDB;Integrated Security=True";
 
+        public List<User> Get_Users()
+        {
+            DALC.DALC oDalc = new DALC.DALC();
+            oDalc.ConnectionString = this.ConnectionString;
+            List<User> oUsers = new List<User>();
+            oUsers = oDalc.Get_Users();
+            
+            return oUsers;
+        }
     }
 }
